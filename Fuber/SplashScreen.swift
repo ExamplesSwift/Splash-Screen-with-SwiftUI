cddff8dd035483779ebd4762cc78b5a68a2a0031
Henry Ambicho T <yiperu@gmail.com>
2019-12-15 16:09:17 -0300

    👨🏽‍💻  Add FuberU struct and re indent all code

--- Fuber/SplashScreen.swift
+++ Fuber/SplashScreen.swift
@@ -43,11 +43,32 @@ struct SplashScreen: View {
     }
     .frame(minWidth: 0, maxWidth: .infinity,
            minHeight: 0, maxHeight: .infinity)
-    .background(fuberBlue)
-    .edgesIgnoringSafeArea(.all)
+      .background(fuberBlue)
+      .edgesIgnoringSafeArea(.all)
   }
 }
+
+struct FuberU: Shape {
+  var percent: Double
   
+  func path(in rect: CGRect) -> Path {
+    let end = percent * 360
+    var p = Path()
+    
+    p.addArc(center: CGPoint(x: rect.size.width/2, y: rect.size.width/2),
+             radius: rect.size.width/2,
+             startAngle: Angle(degrees: 0),
+             endAngle: Angle(degrees: end),
+             clockwise: false)
+    return p
+  }
+  
+  var animatableData: Double {
+    get { return percent }
+    set { percent = newValue }
+  }
+}
+
 #if DEBUG
 struct SplashScreen_Previews : PreviewProvider {
   static var previews: some View {
